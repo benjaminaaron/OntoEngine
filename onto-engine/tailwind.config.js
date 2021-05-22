c0b12b51282d002b1f3a39dd1dbc069f8fcb5a0c
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-22 19:15:07 +0200

    Add custom colors copied from Protege

--- onto-engine/tailwind.config.js
+++ onto-engine/tailwind.config.js
@@ -2,6 +2,29 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      classColor: {
+        DEFAULT: '#cfa500',
+      },
+      objectPropertyColor: {
+        DEFAULT: '#0079ba',
+      },
+      dataPropertyColor: {
+        DEFAULT: '#38a14a',
+      },
+      individualsColor: {
+        DEFAULT: '#874b82',
+      },
+      annotationPropertyColor: {
+        DEFAULT: '#d17a00',
+      },
+      datatypesColor: {
+        DEFAULT: '#ad3b45'
+      },
+      ontologyColor: {
+        DEFAULT: '#6b47a2'
+      }
+    },
     extend: {},
   },
   variants: {
