17d926a6bc25414e8bd8285d311bcda8a3af871d
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-22 12:30:46 +0200

    Cleanup CSS from initial setup

--- onto-engine/src/App.js
+++ onto-engine/src/App.js
@@ -2,13 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          TODO
-        </p>
-      </header>
-    </div>
+    <>
+      TODO
+    </>
   );
 }
 
