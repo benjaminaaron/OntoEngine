0d116059b81cdd1d0370176b41095064523e76b0
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-22 14:33:12 +0200

    Use headless UI for modal dialog
    https://headlessui.dev/react/dialog

--- onto-engine/src/App.js
+++ onto-engine/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import AutocompleteCreatable from './components/AutocompleteCreatable';
+import ModalDialog from './components/ModalDialog';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <div className="text-xl font-medium text-blue-600">Subject</div>
         <input type="text" className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
         <AutocompleteCreatable/>
+        <ModalDialog/>
       </div>
     </>
   );
