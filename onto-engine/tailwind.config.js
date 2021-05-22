1480d5aa113700f2d49c6481678272457a701b87
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-22 20:56:49 +0200

    Extend with custom colors instead of replacing the default palette

--- onto-engine/tailwind.config.js
+++ onto-engine/tailwind.config.js
@@ -3,30 +3,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      classColor: {
-        DEFAULT: '#cfa500',
+    extend: {
+      colors: {
+        classColor: {
+          DEFAULT: '#cfa500',
+        },
+        objectPropertyColor: {
+          DEFAULT: '#0079ba',
+        },
+        dataPropertyColor: {
+          DEFAULT: '#38a14a',
+        },
+        individualsColor: {
+          DEFAULT: '#874b82',
+        },
+        annotationPropertyColor: {
+          DEFAULT: '#d17a00',
+        },
+        datatypesColor: {
+          DEFAULT: '#ad3b45',
+        },
+        ontologyColor: {
+          DEFAULT: '#6b47a2',
+        },
       },
-      objectPropertyColor: {
-        DEFAULT: '#0079ba',
-      },
-      dataPropertyColor: {
-        DEFAULT: '#38a14a',
-      },
-      individualsColor: {
-        DEFAULT: '#874b82',
-      },
-      annotationPropertyColor: {
-        DEFAULT: '#d17a00',
-      },
-      datatypesColor: {
-        DEFAULT: '#ad3b45'
-      },
-      ontologyColor: {
-        DEFAULT: '#6b47a2'
-      }
     },
-    extend: {},
   },
   variants: {
     extend: {},
