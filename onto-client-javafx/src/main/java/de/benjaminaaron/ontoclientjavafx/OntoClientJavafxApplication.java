144efd592bb356bd866fc00584bbf8f59260f4d5
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-31 19:24:21 +0200

    Add new module: onto-client-javafx
    using Spring Initializr in IntelliJ Ultimate with websocket and lombok as dependencies

---
+++ onto-client-javafx/src/main/java/de/benjaminaaron/ontoclientjavafx/OntoClientJavafxApplication.java
@@ -0,0 +1,13 @@
+package de.benjaminaaron.ontoclientjavafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntoClientJavafxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntoClientJavafxApplication.class, args);
+    }
+
+}
---
+++ onto-client-javafx/src/test/java/de/benjaminaaron/ontoclientjavafx/OntoClientJavafxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.benjaminaaron.ontoclientjavafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntoClientJavafxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
