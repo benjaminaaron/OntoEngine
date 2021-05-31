032b3d408d53b64fd6cff0053e432b9f75ec147a
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-31 23:28:36 +0200

    Remove incorrect second @SpringBootApplication, confused the IDE

--- onto-client-javafx/src/main/java/de/benjaminaaron/ontoclientjavafx/OntoClientJavafxApplication.java
+++ onto-client-javafx/src/main/java/de/benjaminaaron/ontoclientjavafx/OntoClientJavafxApplication.java
@@ -2,9 +2,7 @@ package de.benjaminaaron.ontoclientjavafx;
 
 import de.benjaminaaron.ontoclientjavafx.gui.GuiApplication;
 import javafx.application.Application;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class OntoClientJavafxApplication {
 
     public static void main(String[] args) {
