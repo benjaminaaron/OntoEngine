cdca2b2a8c33340fd035fbb01ba7927c6849f48e
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-06-01 00:25:04 +0200

    Use Jena TDB for persistence, get directory from properties file
    In the JenaController constructor, tdbDir didn't get the value from the properties file assigned yet. But when moving the initialization into a @PostConstruct method, Spring had time to collect that value. @PreDestroy ensures that the model will be closed when shutting down the server. Not sure if that is the case in all crashing scenarios though.

--- onto-server/src/main/java/de/benjaminaaron/ontoserver/jena/JenaController.java
+++ onto-server/src/main/java/de/benjaminaaron/ontoserver/jena/JenaController.java
@@ -1,13 +1,25 @@
 package de.benjaminaaron.ontoserver.jena;
 
+import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.tdb.TDBFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class JenaController {
 
-    public JenaController() {
-        Model model = ModelFactory.createDefaultModel();
+    @Value("${jena.tdb.directory}")
+    private String tdbDir;
+    private Model model;
+
+    @PostConstruct
+    private void init() {
+        Dataset dataset = TDBFactory.createDataset(tdbDir) ;
+        model = dataset.getDefaultModel();
 
         Resource sub = model.createResource("http://onto/Benjamin");
         Property pred = model.createProperty("http://onto/isA");
@@ -17,5 +29,10 @@ public class JenaController {
         model.add(statement);
 
         System.out.println(model.listStatements().toSet());
+    }
+
+    @PreDestroy
+    private void close() {
+        model.close();
     }
 }
