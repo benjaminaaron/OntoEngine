630dd9bd77b5ae8ccbcf0ae7483ede754923de29
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-05-31 19:21:11 +0200

    Add new module: onto-server
    using Spring Initializr in IntelliJ Ultimate with websocket and lombok as dependencies

---
+++ onto-server/src/test/java/de/benjaminaaron/ontoserver/OntoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.benjaminaaron.ontoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
