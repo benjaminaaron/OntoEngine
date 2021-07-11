51656c654cbf1e0db731b99323b4175e1dc28bb6
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-07-11 16:20:47 +0200

    Prepare comparison of all vertices with each other

--- onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/GraphSimilarityTask.java
+++ onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/GraphSimilarityTask.java
@@ -1,10 +1,14 @@
 package de.benjaminaaron.ontoserver.suggestion.job.task;
 
+import de.benjaminaaron.ontoserver.model.graph.DirectedMultigraphWithSelfLoops;
+import de.benjaminaaron.ontoserver.model.graph.Edge;
 import de.benjaminaaron.ontoserver.suggestion.Suggestion;
 import de.benjaminaaron.ontoserver.suggestion.job.task.base.JobGraphTask;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraphSimilarityTask extends JobGraphTask {
@@ -13,9 +17,22 @@ public class GraphSimilarityTask extends JobGraphTask {
 
     @Override
     public List<Suggestion> execute() {
+        DirectedMultigraphWithSelfLoops<RDFNode, Edge> graph = graphManager.getGraph();
+        List<RDFNode> vertices = new ArrayList<>(graph.vertexSet());
+        int n = vertices.size();
+        n = n * (n - 1) / 2;
+        logger.info("GraphSimilarityTask: comparing " + n + " pairs, formed from " + vertices.size() + " vertices");
+        for (int a = 0; a < vertices.size() - 1; a++) {
+            RDFNode vertexA = vertices.get(a);
+            for (int b = a + 1; b < vertices.size(); b++) {
+                RDFNode vertexB = vertices.get(b);
+                System.out.println(vertexA + " <--> " + vertexB);
 
-        // TODO
-        
+                // TODO
+            }
+        }
+        // suggestions.add(new Suggestion(null));
+        // compare n pairs of edges too?
         return suggestions;
     }
 }
