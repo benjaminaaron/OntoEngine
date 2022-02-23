3f581de74d07287234984043e26873f8c3c83e9f
benjaminaaron <b.degenhart.work@gmail.com>
2022-02-23 21:48:34 +0100

    Shield against null-statement
    Can happen when there is nothing in the triple store but the periodic job runs

--- onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/base/JobStatementTask.java
+++ onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/base/JobStatementTask.java
@@ -6,6 +6,7 @@ import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 
 import java.util.List;
+import java.util.Objects;
 
 import static de.benjaminaaron.ontoserver.model.Utils.ResourceType.*;
 
@@ -20,10 +21,12 @@ public abstract class JobStatementTask extends JobTask {
 
     @Override
     public List<Suggestion> execute() {
-        check(statement, SUBJECT);
-        check(statement, PREDICATE);
-        if (statement.getObject().isResource()) {
-            check(statement, OBJECT);
+        if (Objects.nonNull(statement)) {
+            check(statement, SUBJECT);
+            check(statement, PREDICATE);
+            if (statement.getObject().isResource()) {
+                check(statement, OBJECT);
+            }
         }
         return suggestions;
     }
