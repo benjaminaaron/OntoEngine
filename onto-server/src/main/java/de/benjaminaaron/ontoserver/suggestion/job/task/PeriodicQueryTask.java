e4b667b2b91c1987db317703d02a8b70dd46de5e
Benjamin Degenhart <b.degenhart.work@gmail.com>
2022-04-03 16:21:22 +0200

    Add first part of querying for periodic queries

--- onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/PeriodicQueryTask.java
+++ onto-server/src/main/java/de/benjaminaaron/ontoserver/suggestion/job/task/PeriodicQueryTask.java
@@ -2,15 +2,15 @@ package de.benjaminaaron.ontoserver.suggestion.job.task;
 
 import de.benjaminaaron.ontoserver.suggestion.Suggestion;
 import de.benjaminaaron.ontoserver.suggestion.job.task.base.JobModelTask;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PeriodicQueryTask extends JobModelTask {
 
@@ -24,7 +24,20 @@ public class PeriodicQueryTask extends JobModelTask {
     public List<Suggestion> execute() {
         List<Suggestion> suggestions = new ArrayList<>();
 
-        // TODO
+        // GET PERIODIC QUERIES
+        Map<String, String> periodicQueries = new HashMap<>();
+        String query = "PREFIX : <http://onto.de/default#> " +
+                "SELECT * WHERE { " +
+                "   ?queryName :hasPeriodicQuery ?query " +
+                "}";
+        try(QueryExecution queryExecution = QueryExecutionFactory.create(query, metaModel)) {
+            ResultSet resultSet = queryExecution.execSelect();
+            while(resultSet.hasNext()) {
+                QuerySolution row = resultSet.next();
+                periodicQueries.put(row.getResource("queryName").getURI(), row.getLiteral("query").getString());
+            }
+        }
+        System.out.println(periodicQueries);
 
         return suggestions;
     }
