cd0bdd44dd06b5c62e260e50c48fe82f411cbe6c
benjaminaaron <b.degenhart.work@gmail.com>
2022-10-30 17:11:54 +0100

    Log triples to console

--- scripts/miro-importer/script.js
+++ scripts/miro-importer/script.js
@@ -7,34 +7,41 @@ const api = new MiroApi(config.ACCESS_TOKEN)
 const app = (async function () {
   const board = await api.getBoard(config.BOARD_ID);
 
-  const ids = {}
+  const nodes = {}
+  const edges = [];
+  const idsOfNodesThatHaveEdges = {}
   let tgf = ""
   let counter = 0
-  const validEdges = [];
-  const connectedNodes = {} // have at least one edge connected
+  let triples = [] // just for console output
 
-  for await (const connector of board.getAllConnectors()) {
-    if (!(connector.startItem && connector.endItem && connector.captions)) continue
-    connectedNodes[connector.startItem.id] = true
-    connectedNodes[connector.endItem.id] = true
-    validEdges.push(connector)
+  for await (const edge of board.getAllConnectors()) {
+    if (!(edge.startItem && edge.endItem && edge.captions)) continue
+    idsOfNodesThatHaveEdges[edge.startItem.id] = true
+    idsOfNodesThatHaveEdges[edge.endItem.id] = true
+    edges.push(edge)
   }
 
   for await (const item of board.getAllItems()) {
-    if (!connectedNodes[item.id]) continue
-    let newId = ++ counter
-    ids[item.id] = newId
-    tgf += newId + " " + clean(item.data.content) + "\n"
+    if (!idsOfNodesThatHaveEdges[item.id]) continue
+    let node = {
+      newId: ++ counter,
+      content: clean(item.data.content)
+    }
+    tgf += node.newId + " " + node.content + "\n"
+    nodes[item.id] = node
   }
 
   tgf += "#\n"
 
-  for (let edge of validEdges) {
-    tgf += ids[edge.startItem.id] + " " + ids[edge.endItem.id]
-           + " " + clean(edge.captions[0].content) + "\n"
+  for (let edge of edges) {
+    const from = nodes[edge.startItem.id]
+    const to = nodes[edge.endItem.id]
+    const edgeContent = clean(edge.captions[0].content)
+    tgf += from.newId + " " + to.newId + " " + edgeContent + "\n"
+    triples.push([from.content, edgeContent, to.content])
   }
 
-  console.log(tgf)
+  console.log(triples)
 
   fs.mkdir(path.join(__dirname, config.TGF_DIR), () => {})
   fs.writeFile(path.join(__dirname, config.TGF_DIR + "/graph.tgf"), tgf, () => {})
