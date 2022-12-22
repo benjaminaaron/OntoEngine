07418b5add53a086793983b0106ba3b8d4632d6f
benjaminaaron <b.degenhart.work@gmail.com>
2022-12-22 16:01:29 +0100

    Create query.html to talk to a new REST endpoint

--- src/main/java/de/benjaminaaron/ontoengine/adapter/primary/RestEndpoints.java
+++ src/main/java/de/benjaminaaron/ontoengine/adapter/primary/RestEndpoints.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,5 +50,11 @@ public class RestEndpoints {
     public ResponseEntity<String> newProject(@RequestBody ProjectCreationInfo projectCreationInfo) {
         // TODO
         return ResponseEntity.ok("");
+    }
+
+    @PutMapping(value = "/query", consumes = "text/plain")
+    public ResponseEntity<String> query(@RequestBody String query) {
+        System.out.println("Query: " + query);
+        return ResponseEntity.ok("OK");
     }
 }
