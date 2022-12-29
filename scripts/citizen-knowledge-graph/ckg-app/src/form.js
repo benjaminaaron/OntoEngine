47a3957d3c3fb783631ae23366c1dd43060da56a
benjaminaaron <b.degenhart.work@gmail.com>
2022-12-29 22:04:55 +0100

    Styling of form

--- scripts/citizen-knowledge-graph/ckg-app/src/form.js
+++ scripts/citizen-knowledge-graph/ckg-app/src/form.js
@@ -22,11 +22,23 @@ function sendFormWorkflow(turtleData) {
       let label = document.createElement('label');
       label.setAttribute('for', fieldId);
       label.innerHTML = field.description;
+      if (field.hasPredicate === 'eVBnumber') {
+        label.innerHTML += ',<a href="#" style="color: yellow; font-weight: bold">mehr Infos</a>';
+      }
       let input = document.createElement('input');
+      input.className = 'form-input-field';
       input.setAttribute('type', 'text');
       input.setAttribute('id', fieldId);
       if (Object.keys(data.valuesFound).includes(field.hasPredicate)) {
+        input.className += ' form-input-field-already-known';
         input.setAttribute('value', data.valuesFound[field.hasPredicate]);
+      }
+      if (field.computableVia && data.valuesFound['birthday']) {
+        input.className += ' form-input-field-computed';
+        // hardwired for the demo
+        let age = 2022 - data.valuesFound['birthday'].split('.')[2];
+        input.setAttribute('value', age + '');
+        label.innerHTML += ",<b style='color: yellow'>berechnet aus Geburtstag</b>";
       }
       reportDiv.appendChild(input);
       reportDiv.appendChild(label);
