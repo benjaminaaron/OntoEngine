1b0595452fe9ac6e089620ad17ee39a2e56fd36f
benjaminaaron <b.degenhart.work@gmail.com>
2022-12-29 13:32:06 +0100

    Construct input fields based on report

--- scripts/citizen-knowledge-graph/ckg-app/src/form.js
+++ scripts/citizen-knowledge-graph/ckg-app/src/form.js
@@ -15,6 +15,23 @@ function sendFormWorkflow(turtleData) {
   .then(response => response.json())
   .then(data => {
     console.log("data:", data);
+    let reportDiv = document.getElementById('reportDiv');
+    let orderedFieldIds = Object.keys(data.fields).sort();
+    for (let fieldId of orderedFieldIds) {
+      let field = data.fields[fieldId];
+      let label = document.createElement('label');
+      label.setAttribute('for', fieldId);
+      label.innerHTML = field.description;
+      let input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      input.setAttribute('id', fieldId);
+      if (Object.keys(data.valuesFound).includes(field.hasPredicate)) {
+        input.setAttribute('value', data.valuesFound[field.hasPredicate]);
+      }
+      reportDiv.appendChild(input);
+      reportDiv.appendChild(label);
+      reportDiv.appendChild(document.createElement('br'));
+    }
   })
   .catch(error => console.error(error))
 }
