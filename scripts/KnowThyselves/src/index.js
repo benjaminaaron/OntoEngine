1711ba4a2f724b1e58514cbe072b2f599019f9c1
benjaminaaron <b.degenhart.work@gmail.com>
2023-03-01 22:06:38 +0100

    Move entire scripting to index.js

--- scripts/KnowThyselves/src/index.js
+++ scripts/KnowThyselves/src/index.js
@@ -1,3 +1,7 @@
 import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
 
 const sparql = new SparqlEndpointFetcher();
+
+window.document.getElementById("query1btn").addEventListener("click", () => {
+  console.log("TODO")
+});
