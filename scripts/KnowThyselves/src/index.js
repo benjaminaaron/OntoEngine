62797b0982fa0370b065e7a74496cba3b1c71abd
benjaminaaron <b.degenhart.work@gmail.com>
2023-03-08 21:39:47 +0100

    Add first tailoredQueryObject

--- scripts/KnowThyselves/src/index.js
+++ scripts/KnowThyselves/src/index.js
@@ -1,34 +1,55 @@
 import { SparqlEndpointFetcher } from "fetch-sparql-endpoint"
-import { decode } from "js-base64";
+import { decode } from "js-base64"
+import { tailoredQueryObjects } from "./tailoredQueryObjects"
 
 const sparql = new SparqlEndpointFetcher()
 const ENDPOINT = "http://localhost:7200/repositories/dev"
 
-async function fetch() {
-  const query = "SELECT * WHERE { ?s ?p ?o }"
-  const bindingsStream = await sparql.fetchBindings(ENDPOINT, query)
-  bindingsStream.on("data", (resultRow) => {
-    let sub = resultRow["s"].value
-    if (sub.startsWith("urn:rdf4j:triple:")) sub = decode(sub.substring(17))
-    let pred = resultRow["p"].value
-    let obj = resultRow["o"].value
-
+async function fetch(tailoredQueryObj, table) {
+  const bindingsStream = await sparql.fetchBindings(ENDPOINT, tailoredQueryObj.query)
+  bindingsStream.on("data", resultRow => {
     console.log(resultRow)
-    console.log(sub, pred, obj)
+    let tr = document.createElement("tr")
+    table.appendChild(tr)
+    for (let header of tailoredQueryObj.tableHeaders) {
+      let value = resultRow[header].value
+      if (value.startsWith("urn:rdf4j:triple:")) value = decode(value.substring(17))
+      let localName = value.split("#").pop()
+      let td = document.createElement("td")
+      td.innerHTML = localName
+      tr.appendChild(td)
+    }
   })
 }
 
 function clearElement(el) {
-  while (el.firstChild) el.removeChild(el.lastChild);
+  while (el.firstChild) el.removeChild(el.lastChild)
 }
 
-function showExample(idx) {
-  clearElement(document.getElementById("root"))
-  //  fetch().then(r => {})
+async function renderTailoredQueryObj(idx) {
+  let tailoredQueryObj = tailoredQueryObjects[idx]
+  let root = document.getElementById("root")
+  clearElement(root)
+  let h3 = document.createElement("h3")
+  h3.innerHTML = tailoredQueryObjects[idx].name
+  root.appendChild(h3)
+  let p = document.createElement("p")
+  p.innerHTML = tailoredQueryObjects[idx].description
+  root.appendChild(p)
+  let table = document.createElement("table")
+  table.style.margin = "0 auto"
+  for (let header of tailoredQueryObj.tableHeaders) {
+    let th = document.createElement("th")
+    th.style.backgroundColor = "lightgray"
+    th.innerHTML = header
+    table.appendChild(th)
+  }
+  root.appendChild(table)
+  await fetch(tailoredQueryObj, table)
 }
 
 function registerClickListener(id, idx) {
-  document.getElementById(id).addEventListener("click", () => showExample(idx))
+  document.getElementById(id).addEventListener("click", () => renderTailoredQueryObj(idx))
 }
 
 registerClickListener("ex1link", 1)
---
+++ scripts/KnowThyselves/src/tailoredQueryObjects.js
@@ -0,0 +1,11 @@
+export const tailoredQueryObjects = {
+  1: {
+    name: "List employees",
+    description: "Gets the names of all employees.",
+    query: "PREFIX : <http://dev.de/default#> "
+        + "SELECT * WHERE { "
+        + "  :DigitalService :hasEmployee ?employee . "
+        + "}",
+    tableHeaders: ["employee"],
+  }
+}
